6689777dacc7fd2e0d2bba04611f74022d735c85
Simon Bengtsson <simonbengt@gmail.com>
2024-01-09 00:06:38 +0100

    Migrate pdf generation

--- components/pdf-service.js
+++ components/pdf-service.js
@@ -1,158 +1,90 @@
 /**
  * Service for handling the competition model
  */
-angular.module('enduroApp').service('pdfgen', ['competition', '$filter', function (comp, $filter) {
-
+angular.module("enduroApp").service("pdfgen", [
+  "competition",
+  "$filter",
+  function (comp, $filter) {
     var self = this;
-    this.START_TYPE = 'start-list';
-    this.RESULT_TYPE = 'results';
+    this.START_TYPE = "start-list";
+    this.RESULT_TYPE = "results";
 
     this.create = function (type) {
-        self.type = type;
-        var title = (type === self.RESULT_TYPE ? 'Resultat ' : 'Startlista ') + comp.name;
-        var def = {
-            footer: footer(),
-            content: [
-                { text: title, style: 'mainHeader' },
-                {
-                    table: table(headers(), data(), widths()),
-                    layout: layout()
-                }
-            ],
-            styles: {
-                tableHeader: {
-                    bold: true,
-                    fontSize: 11
-                },
-                mainHeader: {
-                    fontSize: 20,
-                    lineHeight: 50,
-                    alignment: 'center',
-                    color: '#555',
-                    bold: true,
-                    margin: [0, 0, 0, 5]
-                }
-            },
-            defaultStyle: {
-                fontSize: 9,
-                color: '#333'
-            }
-        };
+      console.log("Hello!");
+      self.type = type;
+      var title =
+        (type === self.RESULT_TYPE ? "Resultat " : "Startlista ") + comp.name;
 
-        if (comp.lapInfoArr.length > 2 && self.RESULT_TYPE === self.type) {
-            def.pageOrientation = 'landscape';
-            def.pageMargins = [40, 30, 40, 50];
-        }
+      var jsPDF = window.jspdf.jsPDF;
+      var doc = new jsPDF("l");
+      var totalPagesExp = "{total_pages_count_string}";
+      doc.autoTable({
+        margin: { top: 25, bottom: 25 },
+        didDrawPage: function (data) {
+          doc.text(title, 15, 18);
 
-        pdfMake.createPdf(def).open();
+          var str =
+            "Sida " +
+            doc.internal.getNumberOfPages() +
+            " av " +
+            totalPagesExp +
+            " · " +
+            new Date().toLocaleString();
+          doc.setFontSize(10);
+
+          // jsPDF 1.4+ uses getHeight, <1.4 uses .height
+          var pageSize = doc.internal.pageSize;
+          var pageHeight = pageSize.getHeight();
+          doc.text(str, data.settings.margin.left, pageHeight - 10);
+        },
+        head: headers(),
+        body: data(),
+      });
+      doc.putTotalPages(totalPagesExp);
+      doc.output("dataurlnewwindow");
+      console.log("Hello2");
     };
 
     function headers() {
-        var lapTitles = [];
-        if (self.type === self.RESULT_TYPE) {
-            comp.lapInfoArr.forEach(function (info) {
-                lapTitles.push(info.title);
-            });
-        }
+      var lapTitles = [];
+      if (self.type === self.RESULT_TYPE) {
+        comp.lapInfoArr.forEach(function (info) {
+          lapTitles.push(info.title);
+        });
+      }
 
-        var columns = [comp.ptId, comp.ptDesc, 'Start'].concat(comp.ptInfoArr).concat(lapTitles);
-        if (self.type === self.RESULT_TYPE) {
-            columns.unshift('Plac');
-            columns.push('Total');
-        }
-        var headers = [];
-        for (var i = 0; i < columns.length; i++) {
-            headers[i] = { text: columns[i], style: 'tableHeader' };
-        }
-        return headers;
+      var columns = [comp.ptId, comp.ptDesc, "Start"]
+        .concat(comp.ptInfoArr)
+        .concat(lapTitles);
+      if (self.type === self.RESULT_TYPE) {
+        columns.unshift("Plac");
+        columns.push("Total");
+      }
+      return [columns];
     }
 
     function data() {
-        var data = [];
-        var pts = $filter('orderBy')(comp.pts, 'plac');
-        pts.forEach(function (pt) {
-            var times = [];
-            if (self.type === self.RESULT_TYPE) {
-                comp.lapInfoArr.forEach(function (info, i) {
-                    times.push(pt.getLapTime(i, info.key) + ' ' + pt.getLapPlac(info.key));
-                });
-            }
-            var start = $filter('date')(comp.getStartTime(pt), 'HH:mm:ss');
-
-            var row = ["" + pt.id, pt.desc, start].concat(pt.info).concat(times);
-            if (self.type === self.RESULT_TYPE) {
-                row.unshift("" + pt.plac);
-                row.push(pt.getTotal());
-            }
-            data.push(row);
-        });
-        return data;
-    }
-
-    function widths() {
-        var lapsWidths = [];
+      var data = [];
+      var pts = $filter("orderBy")(comp.pts, "plac");
+      pts.forEach(function (pt) {
+        var times = [];
         if (self.type === self.RESULT_TYPE) {
-            comp.lapInfoArr.forEach(function () {
-                lapsWidths.push('auto');
-            });
+          comp.lapInfoArr.forEach(function (info, i) {
+            times.push(
+              pt.getLapTime(i, info.key) + " " + pt.getLapPlac(info.key)
+            );
+          });
         }
-        var infoWidths = [];
-        comp.ptInfoArr.forEach(function () {
-            infoWidths.push('*');
-        });
-        var widths = ['auto', 'auto', 'auto'].concat(infoWidths).concat(lapsWidths);
+        var start = $filter("date")(comp.getStartTime(pt), "HH:mm:ss");
+
+        var row = ["" + pt.id, pt.desc, start].concat(pt.info).concat(times);
         if (self.type === self.RESULT_TYPE) {
-            widths.unshift('auto');
-            widths.push('auto');
+          row.unshift("" + pt.plac);
+          row.push(pt.getTotal());
         }
-        return widths;
+        data.push(row);
+      });
+      return data;
     }
-
-    function footer() {
-        return function (currentPage, pageCount) {
-            return {
-                columns: [
-                    { text: $filter('date')(new Date(), 'yyyy-MM-dd HH:mm'), margin: [50, 10] },
-                    { text: 'Sida ' + currentPage + ' av ' + pageCount, alignment: 'right', margin: [50, 10]}
-                ]
-            };
-        };
-    }
-
-    function layout() {
-        return {
-            hLineWidth: function (i, node) {
-                if (i === 0 || i === node.table.body.length) return 0;
-                return 1;
-            },
-            vLineWidth: function (i) {
-                return 0;
-            },
-            hLineColor: function (i) {
-                return i === 1 ? 'black' : '#aaa';
-            },
-            paddingTop: function (i) {
-                return 2;
-            },
-            paddingBottom: function (i) {
-                return 2;
-            },
-            paddingLeft: function (i) {
-                return i === 0 ? 0 : 6;
-            },
-            paddingRight: function (i, node) {
-                return (i === node.table.widths.length - 1) ? 0 : 6;
-            }
-        };
-    }
-
-    function table(headers, data, widths) {
-        data.unshift(headers);
-        return {
-            headerRows: 1,
-            widths: widths,
-            body: data
-        };
-    }
-
-}]);
+  },
+]);
